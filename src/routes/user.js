d52078051b4212b8cfa36bfe17af91fa2e0cb0c3
SARANKUMARTSK <sarankumartsk@gmail.com>
2024-05-22 18:30:51 +0530

    user edit

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,7 +8,7 @@ router.get('/:id',AdminGaurd,UserController.getUserById)
 router.post('/',UserController.createUser)
 router.post('/login',UserController.login)
 router.delete('/:id',AdminGaurd,UserController.deleteUserById)
-router.put('/:id', UserController.editUserById)
+router.put('/edit/:id', UserController.editUserById)
 router.post('/forgotPassword',UserController.forgotMail)
 router.post('/resetPassword/:token',UserController.resetPassword)
 
